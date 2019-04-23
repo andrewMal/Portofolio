ed43b1e62099d43d44c08ef87eb12d9e8be8c6df
andreas malliotakis <amalliot4@gmail.com>
2019-04-24 01:25:20 +0300

    :q! :wq

--- CombinationCalculator.java
+++ CombinationCalculator.java
@@ -2,11 +2,13 @@ package lottery;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class CombinationCalculator {
 	
-	public void findAllCombination(int[] arr) {
+	public static void findAllCombination(int[] arr) {
 		try(PrintWriter pw=new PrintWriter(new FileOutputStream("D://LotterySystem.txt"),true)){
 			int n=arr.length-1;
 			int v=1;
@@ -31,6 +33,7 @@ public class CombinationCalculator {
 					}
 				}
 			}
+			pw.print(-1);
 		
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -39,26 +42,138 @@ public class CombinationCalculator {
 	/**
 	 * filter for group of six with maximum 4 odd numbers 
 	 * @param file with group of six numbers
+	 * @param int i is maximum number of even which want to have in every group of six
 	 */
-	public void checkForEven(File file) {
-		
+	
+	public static void checkForEven(File file,int maxEven) {
+		try(Scanner in=new Scanner(file);PrintWriter pw=new PrintWriter(new FileOutputStream("D://temp.txt"),true)){
+			int[] lotteryComb=new int[6];
+			String num;
+			int counter=0;
+			while(in.hasNext()){
+				num=in.next();
+				if(num.equals("-1")) break;
+				for(int i=0;i<lotteryComb.length;i++) {
+					lotteryComb[i]=in.nextInt();
+					if(lotteryComb[i]%2!=0) counter++;
+				}
+				if(counter<=maxEven) {
+					for(int j=0;j<lotteryComb.length;j++) {
+						pw.print(lotteryComb[j]+" ");
+					}
+					pw.println();
+					counter=0;
+				}
+				else counter=0;
+			}
+			pw.print("-1");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	/**
+	 * 
 	 * filter for group of six with maximum 4 odd numbers 
 	 * @param file with group of six numbers
 	 */
-	public void checkForOdd(File file) {
-		
+	public void checkForOdd(File file,int maxOdd) {
+		try(Scanner in=new Scanner(file);PrintWriter pw=new PrintWriter(new FileOutputStream("D://temp.txt"),true)){
+			int[] lotteryComb=new int[6];
+			String num;
+			int counter=0;
+			while(in.hasNext()){
+				num=in.next();
+				if(num.equals("-1")) break;
+				for(int i=0;i<lotteryComb.length;i++) {
+					lotteryComb[i]=in.nextInt();
+					if(lotteryComb[i]%2==0) counter++;
+				}
+				if(counter<=maxOdd) {
+					for(int j=0;j<lotteryComb.length;j++) {
+						pw.print(lotteryComb[j]+" ");
+					}
+					pw.println();
+					counter=0;
+				}
+				else counter=0;
+			}
+			pw.print("-1");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	/**
-	 * filter which catch if the group of six contains three (or more) numbers in a row
+	 * filter which catch if the group of six contains numbers in a row
 	 * @param file with group of six numbers
 	 */
-	public void checkForNumInRow(File file) {
-		
+	public void checkForNumInRow(File file,int maxNumRow) {
+		try(Scanner in=new Scanner(file);PrintWriter pw=new PrintWriter(new FileOutputStream("D://temp.txt"),true)){
+			int[] lotteryComb=new int[6];
+			String num;
+			int counter=0;
+			while(in.hasNext()){
+				num=in.next();
+				if(num.equals("-1")) break;
+				for(int i=0;i<lotteryComb.length;i++) {
+					lotteryComb[i]=in.nextInt();
+					if(i>=1 && lotteryComb[i]-lotteryComb[i-1]==1) counter++;
+				}
+				if(counter<=maxNumRow) {
+					for(int j=0;j<lotteryComb.length;j++) {
+						pw.print(lotteryComb[j]+" ");
+					}
+					pw.println();
+					counter=0;
+				}
+				else counter=0;
+			}
+			pw.print("-1");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 
+	 * @param file
+	 */
+	public void filterForDecade(File file,int maxSameDecade) {
+		try(Scanner in=new Scanner(file);PrintWriter pw=new PrintWriter(new FileOutputStream("D://temp.txt"),true)){
+			int[] lotteryComb=new int[6];
+			int[] decades=new int[10];
+			boolean higherToMax=false;
+			String num;
+			int counter=0;
+			while(in.hasNext()){
+				num=in.next();
+				if(num.equals("-1")) break;
+				for(int i=0;i<lotteryComb.length;i++) {
+					lotteryComb[i]=in.nextInt();
+				}
+				for(int i=0;i<lotteryComb.length;i++) {
+					decades[lotteryComb[i]/10]++;
+				}
+				for(int i=0;i<decades.length;i++) {
+					if(decades[i]>maxSameDecade) {
+						higherToMax=true;
+					}
+					decades[i]=0; //turn to zero the cells of array (counters)
+				}
+				if(!higherToMax) {
+					for(int j=0;j<lotteryComb.length;j++) {
+						pw.print(lotteryComb[j]+" ");
+					}
+					pw.println();
+					//counter=0;
+				}
+				//else counter=0;
+			}
+			pw.print("-1");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void filterForDecade(File file) {
+	public void printFile(File file) {
 		
 	}
 }
--- LotteryDemo.java
+++ LotteryDemo.java
@@ -1,11 +1,48 @@
 package lottery;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class LotteryDemo {
 
 	public static void main(String[] args) {
+		int[] arr= {2,8,15,26,32,39,46,48};
+		
+		CombinationCalculator.findAllCombination(arr);
+		
+		File file =new File("D://LotterySystem.txt");
+		
+		CombinationCalculator.checkForEven(file,1);
+		
+//		Scanner in;
+//		try {
+//			in = new Scanner(file);
+//			int[] lotteryComb=new int[6];
+//			String num;
+//			num=in.next();
+//			lotteryComb[0]=in.nextInt();
+//			lotteryComb[1]=in.nextInt();
+//			lotteryComb[2]=in.nextInt();
+//			lotteryComb[3]=in.nextInt();
+//			lotteryComb[4]=in.nextInt();
+//			lotteryComb[5]=in.nextInt();
+//			
+//			System.out.println(num);
+//			System.out.println(lotteryComb[0]);
+//			System.out.println(lotteryComb[1]);
+//			System.out.println(lotteryComb[2]);
+//			System.out.println(lotteryComb[3]);
+//			System.out.println(lotteryComb[4]);
+//			System.out.println(lotteryComb[5]);
+//			System.out.println(in.next());
+//			System.out.println(in.next());
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		
 	}
