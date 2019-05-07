e0a3cdc11506177bde784a4624a51cecf0af40b5
andreas malliotakis <amalliot4@gmail.com>
2019-05-07 23:03:46 +0300

    first commit

---
+++ DoublyList.java
@@ -0,0 +1,156 @@
+package doublyLinkedList;
+
+public class DoublyList {
+	
+	private ListNode head;
+	private ListNode tail;
+	int listSize = 0;
+	
+	public DoublyList() {
+		this.head=null;
+		this.tail=null;
+	}
+	
+	public void insertFirst(char c) {
+		ListNode  node = new ListNode();
+		node.data=c;
+		node.n++;
+		node.next=head;
+		node.prev=null;
+		
+		if(isEmpty()) {
+			tail=node;
+			head=node;
+		}
+		else head=node;
+		
+		listSize++;
+	}
+	//deference****************
+	public void removeFirst() {
+		if(isEmpty())System.out.println("The List is Empty");
+		else {
+			head=head.next;
+			head.prev=null;
+		}
+		listSize--;
+	}
+	
+	public void insertLast(char c) {
+		if(isEmpty()) insertFirst(c);
+		else {
+			ListNode node = new ListNode();
+			
+			tail.next=node;
+			
+			node.data=c;
+			node.n++;
+			node.prev=tail;
+			node.next=null;
+			
+			tail=node;
+			
+			listSize++;
+		}
+		
+	}
+	
+	public void removeLast() {
+		if(isEmpty()) System.out.println("The list is Empty");
+		else if(head.next==null) removeFirst();
+		else {
+			tail=tail.prev;
+			tail.next=null;
+		}
+		listSize--;
+	}
+	public ListNode charExists(char c,ListNode pivot) {
+		boolean found=false;
+		pivot=null;
+		if(!isEmpty()) {
+			ListNode node=head;
+			while(!found && node!=null) {
+				if(node.data==c) {
+					found=true;
+					pivot=node;
+				}
+				else node=node.next;
+			}
+		}
+		return pivot;
+	}
+	
+	public void traverse(float plentyChars) {
+		for(ListNode i=head; i!=null; i=i.next) {
+			if(!(i.data==' ')) System.out.print(i.data+" ");
+			else System.out.print("space"+" ");
+			System.out.println((i.n/plentyChars)*100 +"%");
+		}
+	}
+	//************
+	public void increaseCharNum(ListNode listNode) {
+		listNode.n++;
+	}
+	
+	public void bubbleChars() {
+		ListNode j=null;
+		ListNode piv=null;
+		char min;
+		
+		for(ListNode i=head; i!=null; i=i.next) {
+			min= (char) 127;
+			for(j=i;j!=null;j=j.next) {
+				if(j.data<min) {
+					min=j.data;
+					piv=j;
+				}
+			}
+			swap(i,piv);
+		}
+	}
+	
+	public void bubbleFrequencies() {
+		ListNode j=null;
+		ListNode piv=null;
+		
+		for (ListNode i=head;i!=null;i=i.next) {
+			int max =Integer.MAX_VALUE;
+			for(j=i;j!=null;j=j.next) {
+				if(j.n>max) {
+					max=j.n;
+					piv=j;
+				}
+			}
+			swap(i,piv);
+		}
+	}
+	
+	static void swap(ListNode i,ListNode j) {
+		char tmpChar=i.data;
+		int tmpNum=i.n;
+		
+		i.data=j.data;
+		i.n=j.n;
+		j.data=tmpChar;
+		j.n=tmpNum;
+	}
+	
+	private boolean isEmpty() {
+		
+		if(head==null && tail==null) return true;
+		return false;
+	}
+	
+	public int size() {
+		return listSize;
+	}
+	
+}
+
+
+class ListNode{
+	public char data;
+	public int n;
+	public ListNode next;
+	public ListNode prev;
+}
---
+++ FileFrequencyDemo.java
@@ -0,0 +1,51 @@
+package doublyLinkedList;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class FileFrequencyDemo {
+
+	public static void main(String[] args) {
+		try {
+			File file=new File("D://data.txt");
+			if(!file.exists()) {
+				System.out.println("File not found");
+				System.exit(-1);
+			}
+			FileReader in = new FileReader(file);
+			DoublyList myList=new DoublyList();
+			ListNode pivot=null;
+			char ch;
+			int intTemp;
+			float totalChars = 0;
+			while((intTemp=in.read())!=-1) {
+				if(intTemp!=13) {
+					ch=(char) intTemp;
+					if(myList.charExists(ch, pivot)!=null) myList.increaseCharNum(myList.charExists(ch, pivot));
+					else myList.insertLast(ch);
+					totalChars++;
+				}
+				else in.read();
+				
+				
+				//***************
+			}
+			
+			System.out.println("Sort by chars");
+			myList.bubbleChars();
+			myList.traverse(totalChars);
+			
+			System.out.println("Sort by frequency");
+			myList.bubbleFrequencies();
+			myList.traverse(totalChars);
+			
+			in.close();
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
